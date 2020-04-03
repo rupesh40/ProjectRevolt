46be28595def9595ca81c3c52209e331b37e468c
farqtech <ikramf90@gmail.com>
2020-04-03 16:03:43 +0530

    updated project files

--- GridOwner/app.js
+++
@@ -1,5 +0,0 @@
-var user1= require('./index');
-const owner = new user1.GridOwner();
-owner.Demand_on_grid();
-owner.SlotBooking_backside();
-owner.Charging_station();
---
+++ GridOwner/models/user.js
@@ -0,0 +1,28 @@
+/**
+ * For now leave this class as it is
+ * we'll modify it as we proceed
+ */
+class User {
+
+    User(args) {
+        this.id = args.id;
+    }
+
+    maps() {
+        console.log("function for maps");
+    }
+
+    slotBooking() {
+        console.log("function for slot booking");
+    }
+
+    rangeAnxiety() {
+        console.log("function for range anxiety solution");
+    }
+
+    tripPlanner() {
+        console.log("function for trip planner");
+    }
+}
+
+module.exports = User; 
