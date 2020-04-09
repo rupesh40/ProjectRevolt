9441fde2763f0964dc7aa74e75d349279fb73ef4
farqtech <ikramf90@gmail.com>
2020-04-09 13:30:50 +0530

    updated user service

--- services/userService.js
+++ services/userService.js
@@ -1,15 +1,3 @@
 const UserModel  = require('../models/user');
 
-class UserService {
-    // update this method to get list of all users from db
-  getUsers() {
-    const userRecord = UserModel.createUser({});
-    return userRecord;
-  }
-
-  saveUser(){
-    // update to save user objects in db
-  }
-}
-
-module.exports = new UserService(); 
+// more complex logic will be added here. e.g. calculating user stats, user login, identifying current logged in user, etc
