32e7df91c1b80be15d6ea27b3cf4ace861e10bb8
rupesh <rupeshspund@gmail.com>
2020-07-10 14:27:28 +0530

    added some login functionalities

--- front-end/src/components/login&registration/Userlog/login.jsx
+++ front-end/src/components/login&registration/Userlog/login.jsx
@@ -4,33 +4,60 @@ import {Link} from 'react-router-dom';
 import "../Styling/style.scss";
 import "../Styling/App.scss";
 import 'bootstrap/dist/css/bootstrap.css'
+import login from "../../Backend/user"
 export  class Login extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state={
+      email: "",
+      password: "",
+
+    }
   }
   change=()=>{
     this.props.check();
 }
+changeEmail = e =>{
+  this.setState({email:e.target.value})
+}
+changePassword=e=>{
+  this.setState({password:e.target.value})
+}
+onSubmit=e=>{
+  e.preventDefault()
+const data={
+
+email:this.state.email,
+password:this.state.password
+}
+login(data) 
+
+
+
+}
   render() {
     return (
       <div className="base-container" ref={this.props.containerRef}>
-     
+        <form /*onSubmit={this.onSubmit}*/>
         <div className="header">User Login</div>
         <div className="content">
         
           <div className="image">
             <img src={loginImg} />
           </div>
+          
           <div className="form">
             <div className="form-group">
-              <label htmlFor="username">Username</label>
-              <input type="text" name="username" placeholder="username" className="form-control form-control-sm validate" />
+              <label htmlFor="email">Email</label>
+              <input type="text" name="email" placeholder="email" className="form-control form-control-sm validate" onChange={this.changeEmail} value={this.state.email}/>
             </div>
             <div className="form-group">
               <label htmlFor="password">Password</label>
-              <input type="password" name="password" placeholder="password" className="form-control form-control-sm validate"  />
+              <input type="password" name="password" placeholder="password" className="form-control form-control-sm validate" onChange={this.changePassword} value={this.state.password} />
             </div>
           </div>
+          
        
         <div className="footer">
           <button type="button" onClick={this.change} className="btn1" >
@@ -42,6 +69,7 @@ export  class Login extends React.Component {
                 </p>
         </div>
       </div>
+      </form>
       </div>
     );
   }
